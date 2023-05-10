0b04fd61b6f3dcaddff6c1855cb28266fc640686
Garrett Primm <garrett.primm@abbvie.com>
2023-05-10 22:22:04 +0000

    chore: Add unit tests

---
+++ tests/test_api.py
@@ -0,0 +1,35 @@
+import pytest
+import requests
+from crxcavator import api
+
+extension_id = "lkhghipfmlbmmcamcamkhpjjggnlpani"
+extension_version = "10.1.2"
+
+
+class MockResponse:
+    @staticmethod
+    def json():
+        return {"mock_key": "mock_response"}
+
+@pytest.fixture
+def mock_get_report(monkeypatch):
+
+    def mock_get(*args, **kwargs):
+        return MockResponse()
+
+    monkeypatch.setattr(requests, "get", mock_get)
+
+@pytest.fixture
+def mock_get_all_reports(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return MockResponse()
+    
+    monkeypatch.setattr(requests, "get", mock_get)
+
+def test_get_report(mock_get_report):
+    result = api.get_report(extension_id, extension_version)
+    assert type(result) is dict  
+
+def test_get_all_reports(mock_get_report):
+    result = api.get_report(extension_id, extension_version)
+    assert type(result) is list
