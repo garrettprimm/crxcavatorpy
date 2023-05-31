5d9f77b34b39aa5ce281d646f3e692c11a51f100
Garrett Primm <garrett.primm@abbvie.com>
2023-05-31 17:16:49 +0000

    release: init repo

---
+++ tests/test_api.py
@@ -0,0 +1,44 @@
+import pytest
+import requests
+from crxcavator import api
+
+extension_id = "lkhghipfmlbmmcamcamkhpjjggnlpani"
+extension_version = "10.1.2"
+
+
+class ReportResponse:
+    @staticmethod
+    def json():
+        return {"mock_key": "mock_response"}
+
+
+class AllReportsResponse:
+    @staticmethod
+    def json():
+        return [{"mock_key": "mock_response"}]
+
+
+@pytest.fixture
+def mock_get_report(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return ReportResponse()
+
+    monkeypatch.setattr(requests, "get", mock_get)
+
+
+@pytest.fixture
+def mock_get_all_reports(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return AllReportsResponse()
+
+    monkeypatch.setattr(requests, "get", mock_get)
+
+
+def test_get_report(mock_get_report):
+    result = api.get_report(extension_id, extension_version)
+    assert type(result) is dict
+
+
+def test_get_all_reports(mock_get_all_reports):
+    result = api.get_report(extension_id, extension_version)
+    assert type(result) is list
